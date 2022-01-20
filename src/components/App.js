596bf6a327df952acefe4051a837e06aa9cb987a
lnuvy <hangeulgod@naver.com>
2022-01-20 14:28:11 +0900

    구글회원가입 / 로그인 firebase9v

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
-import AppRouter from "./Router";
+import React, { useEffect, useState } from "react";
+import AppRouter from "components/Router";
 import { authService } from "fbase";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  console.log(authService.currentUser);
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    onAuthStateChanged(authService, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+        setInit(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+  }, []);
+
   return <AppRouter isLoggedIn={isLoggedIn} />;
 }
 
