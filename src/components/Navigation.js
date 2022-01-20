a1da9f66d9064c1894715286fd7e48f46fa0348b
lnuvy <hangeulgod@naver.com>
2022-01-20 15:40:19 +0900

    Link to 로 이동 확인

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <nav>
       <ul>
-        <li>ggg</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">My profile</Link>
+        </li>
       </ul>
     </nav>
   );
