d0e4ebb19dd8eebe5acdf0f50b9fb25980a5e936
lnuvy <hangeulgod@naver.com>
2022-01-21 14:58:41 +0900

    작동확인

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,39 +1,18 @@
 import Tweet from "components/Tweet";
-import { dbService, storageService } from "fbase";
+import Imymemine from "components/TweetFactory";
 import {
-  addDoc,
   collection,
-  getDocs,
   getFirestore,
   onSnapshot,
   orderBy,
   query,
 } from "firebase/firestore";
-import { getDownloadURL, ref, uploadString } from "firebase/storage";
-import React, { useEffect, useRef, useState } from "react";
-import { v4 } from "uuid";
+import React, { useEffect, useState } from "react";
 
 const Home = ({ userObj }) => {
-  const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
-  const [attachment, setAttachment] = useState("");
-
-  const fileInput = useRef();
-
-  // const getTweets = async () => {
-  //   const q = query(collection(dbService, "tweets"));
-  //   const querySnapshot = await getDocs(q);
-  //   querySnapshot.forEach((doc) => {
-  //     const tweetObj = {
-  //       ...doc.data(),
-  //       id: doc.id,
-  //     };
-  //     setTweets((prev) => [tweetObj, ...prev]);
-  //   });
-  // };
 
   useEffect(() => {
-    // getTweets();
     const q = query(
       collection(getFirestore(), "tweets"),
       orderBy("createdAt", "desc")
@@ -53,82 +32,10 @@ const Home = ({ userObj }) => {
       unsubscribe();
     };
   }, []);
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    let attachmentUrl = "";
-
-    if (attachment !== "") {
-      const fileRef = ref(storageService, `${userObj.uid}/${v4()}`);
-      const response = await uploadString(fileRef, attachment, "data_url");
-      console.log(response);
-      attachmentUrl = await getDownloadURL(fileRef);
-    }
-
-    try {
-      const docRef = await addDoc(collection(dbService, "tweets"), {
-        text: tweet,
-        createdAt: Date.now(),
-        creatorId: userObj.uid,
-        attachmentUrl,
-      });
-      setTweet(docRef);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setTweet(value);
-  };
-
-  const onFileChange = (event) => {
-    const {
-      target: { files },
-    } = event;
-    const thisFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = (finishedEvent) => {
-      console.log(finishedEvent);
-      const {
-        target: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    reader.readAsDataURL(thisFile);
-  };
-
-  const onClearPhoto = () => {
-    fileInput.current.value = null;
-    setAttachment("");
-  };
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          // value={tweet}
-          onChange={onChange}
-          type="text"
-          placeholder="What's on your mind?"
-          maxLength={120}
-        />
-        <input
-          type="file"
-          accept="image/*"
-          onChange={onFileChange}
-          ref={fileInput}
-        />
-        <input type="submit" value="tweet" />
-        {attachment && (
-          <div>
-            <img src={attachment} width="50px" heigth="40px" alt="upload" />
-            <button onClick={onClearPhoto}>Clear</button>
-          </div>
-        )}
-      </form>
+      <Imymemine userObj={userObj} />
       <div>
         <br />
         <hr />
