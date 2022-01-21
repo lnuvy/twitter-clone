5b40a048c1772389a61b6ce4c48dc147ab697933
lnuvy <hangeulgod@naver.com>
2022-01-21 09:53:38 +0900

    App.js리팩토링

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,12 @@ import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
 
   useEffect(() => {
     onAuthStateChanged(authService, (user) => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj(user);
-        console.log(user);
-      } else {
-        setIsLoggedIn(false);
       }
       setInit(true);
     });
@@ -23,7 +18,7 @@ function App() {
 
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+      <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
       <footer>&copyright</footer>
     </>
   );
